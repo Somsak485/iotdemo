4d71f93e220c4bcea3ef8ec719bd5fb305a87e76
Somsak485 <55083114+Somsak485@users.noreply.github.com>
2019-09-10 23:18:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-    <button id="btn">Test</button>
+    <button id="btn">Show Chart</button>
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -22,21 +23,66 @@
     <script type="text/javascript">
       $( document ).ready(function(){
         $("#btn").click(function(){
-             var url ="https://api.thingspeak.com/channels/860699/feeds.json?results=50";
-                $.ajax(
+
+            var dataPoints_f1=[];
+            var dataPoints_f2=[];
+            var url ="https://api.thingspeak.com/channels/860699/feeds.json?results=50";
+
+            $.ajax(
                     {
 
-                    url: url, 
-                    type: 'get', 
-                    dataType: 'json',
-                    success: function(feedback){
-                        alert(JSON.stringify(feedback.feeds));
-                                            
-                    }          
-                });
-            
+                      url: url, 
+                      type: 'get', 
+                      dataType: 'json',
+                      success: function(feedback){
+                            //alert(JSON.stringify(feedback.feeds));
+                            
+                            $.each(feedback.feeds,function(k,item){
+                                //alert(JSON.stringify(item.field1)); 
+                                dataPoints_f1.push({
+                                    y:parseInt(item.field1)
+                                });
+                                dataPoints_f2.push({
+                                    y:parseInt(item.field2)
+                                });  
+                               
+
+                            });
+                            //alert(JSON.stringify(dataPoints_f1));      
+                                                                          
+                            var chart = new CanvasJS.Chart("chartContainer", {
+                              animationEnabled: true,
+                              theme: "light1",
+                              title:{
+                                text: "Simple Line Chart"
+                              },
+                              axisY:{
+                                includeZero: false
+                              },
+                              data: [
+                                      {        
+                                        type: "line",       
+                                        dataPoints:dataPoints_f1  
+                                      },
+                                  
+                                      {        
+                                        type: "line",       
+                                        dataPoints: dataPoints_f2
+                                      }
+                                   
+                                  ]
+                            });
+             
+                            chart.render();  
+
+                          
+                      } 
+
+          });
+
         });
       });
     </script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
   </body>
 </html>
